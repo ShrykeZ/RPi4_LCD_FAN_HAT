3d5000b19377398cb3abfc4066693121411862b9
Mike <9383604+ShrykeZ@users.noreply.github.com>
2024-02-10 18:46:49 +0000

    Initial Build

---
+++ lib/RPi_FLH/SSD1306.py
@@ -0,0 +1,92 @@
+from smbus import SMBus
+import time
+import numpy as np
+
+class SSD1306(object):
+    def __init__(self, width=128, height=32, addr=0x3c):
+        self.width = width
+        self.height = height
+        self.Column = width
+        self.Page = int(height/8)
+        self.addr = addr
+        self.bus = SMBus(1)
+
+    def SendCommand(self, cmd):
+        self.bus.write_byte_data(self.addr, 0x00, cmd)
+
+    def SendData(self, cmd):
+        self.bus.write_byte_data(self.addr, 0x40, cmd)
+
+    def Closebus(self):
+        self.bus.close()
+
+    def Init(self):
+        self.SendCommand(0xAE)
+        self.SendCommand(0x40) # Low
+        self.SendCommand(0xB0) # High
+        self.SendCommand(0xC8) # !Offset
+        self.SendCommand(0x81)
+        self.SendCommand(0xff)
+        self.SendCommand(0xa1)
+        self.SendCommand(0xa6)
+        self.SendCommand(0xa8)
+        self.SendCommand(0x1f)
+        self.SendCommand(0xd3)
+        self.SendCommand(0x00)
+        self.SendCommand(0xd5)
+        self.SendCommand(0xf0)
+        self.SendCommand(0xd9)
+        self.SendCommand(0x22)
+        self.SendCommand(0xda)
+        self.SendCommand(0x02)
+        self.SendCommand(0xdb)
+        self.SendCommand(0x49)
+        self.SendCommand(0x8d)
+        self.SendCommand(0x14)
+        self.SendCommand(0xaf)
+
+    def ClearBlack(self):
+        for i in range(0, self.Page):
+            self.SendCommand(0xb0 + i)
+            self.SendCommand(0x00)
+            self.SendCommand(0x10) 
+            for j in range(0, self.Column):
+                self.SendData(0x00)
+
+    def ClearWhite(self):
+        for i in range(0, self.Page):
+            self.SendCommand(0xb0 + i)
+            self.SendCommand(0x00)
+            self.SendCommand(0x10) 
+            for j in range(0, self.Column):
+                self.SendData(0xff)
+
+    def getbuffer(self, image):
+        buf = [0xff] * (self.Page * self.Column)
+        image_monocolor = image.convert('1')
+        imwidth, imheight = image_monocolor.size
+        pixels = image_monocolor.load()
+        if(imwidth == self.width and imheight == self.height):
+            for y in range(imheight):
+                for x in range(imwidth):
+                    if pixels[x, y] == 0:
+                        buf[x + int(y / 8) * self.width] &= ~(1 << (y % 8))
+        elif(imwidth == self.height and imheight == self.width):
+            for y in range(imheight):
+                for x in range(imwidth):
+                    newx = y
+                    newy = self.height - x - 1
+                    if pixels[x, y] == 0:
+                        buf[(newx + int(newy / 8 )*self.width) ] &= ~(1 << (y % 8))
+        for x in range(self.Page * self.Column):
+            buf[x] = ~buf[x]
+        return buf
+            
+    def ShowImage(self, pBuf):
+        for i in range(0, self.Page):            
+            self.SendCommand(0xB0 + i)
+            self.SendCommand(0x00)
+            self.SendCommand(0x10)
+            for j in range(0, self.Column):
+                self.SendData(pBuf[j+self.width*i])
+
---
+++ main.py
@@ -0,0 +1,23 @@
+import time
+import sys
+import os
+
+libdir = "./lib/"
+
+import logging
+
+from RPi_FLH import RPi_FLH
+
+ 
+logging.basicConfig(level=logging.INFO)
+
+POE = RPi_FLH.RPi_FLH()
+        
+try:  
+    while(1):
+        POE.POE_HAT_Display(43)
+        time.sleep(1)
+        
+except KeyboardInterrupt:    
+    print("ctrl + c:")
+    POE.FAN_OFF()
